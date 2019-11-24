37dec47c0bb7ad62d64d19cdc9df9944ff2b6cc1
Chanwit Kaewkasi <chanwit@gmail.com>
2019-11-24 15:03:00 +0700

    prepare new notebook images

---
+++ notebook-image/build.sh
@@ -0,0 +1,3 @@
+docker build -t weaveworks/kubeflow-jupyterlab-kale:tensorflow-1.14.0-cpu_kubeflow-v0.7.0_b1 .
+docker build -t chanwit/kubeflow-jupyterlab-kale:tensorflow-1.14.0-cpu_kubeflow-v0.7.0_b1 .
+docker push chanwit/kubeflow-jupyterlab-kale:tensorflow-1.14.0-cpu_kubeflow-v0.7.0_b1
